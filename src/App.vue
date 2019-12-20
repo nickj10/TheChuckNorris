476e4488dc06e9be1a2d2f5e29f7eaa33689dc9e
Nicole Marie Jimenez <jas.jimenez411@gmail.com>
2019-12-20 15:04:53 +0100

    Create Homepage view

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <NotFound />ˆ -->
+    <Homepage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import NotFound from '@/views/NotFound'
+import Homepage from '@/views/Home'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Homepage,
   }
 }
 </script>
