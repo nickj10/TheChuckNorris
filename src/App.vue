d4ccf8a0ff38760c7675c889a9bd1e2df6f1e03f
Nicole Marie Jimenez <jas.jimenez411@gmail.com>
2019-12-26 16:34:13 +0100

    Configure vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <!-- <NotFound />ˆ -->
-    <Homepage />
+  <div id="app" class="wrapper">
+    <!-- ADD HEADER COMPONENT HERE-->
+    <div class="app-container">
+      <component v-if="component" :is="component" />
+      <router-view v-else />
+    </div>
+    <!-- ADD FOOTER COMPONENT HERE -->
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-//import NotFound from '@/views/NotFound'
 import Homepage from '@/views/Home'
 
 export default {
   name: 'app',
   components: {
     Homepage,
+  },
+  metaInfo() {
+    return {
+      titleTemplate: title =>
+        title ? `${title} | The Chuck Norris` : `The Chuck Norris`,
+    }
+  },
+  data() {
+    return {
+      component: false,
+    }
+  },
+  watch: {
+    $route: 'onRouteChange',
+  },
+  methods: {
+    onRouteChange() {
+      this.component = false;
+    },
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
