652f80ed6078d60b72db31ad697755f0b54b1450
Nicole Marie Jimenez <jas.jimenez411@gmail.com>
2019-12-27 22:59:50 +0100

    feat(#18): Route to Gallery

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
+import Gallery from '@/views/Gallery'
 import NotFound from '@/views/NotFound'
 
 Vue.use(Router)
@@ -12,6 +13,11 @@ export default new Router({
             path: '/',
             name: 'home',
             component: Home,
+        },
+        {
+            path: '/gallery',
+            name: 'gallery',
+            component: Gallery,
         },
         { path: '*', component: NotFound },
     ],
---
+++ src/views/Gallery/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="gallery">
+        <div class="gallery__title">
+            Chuck's Gallery
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+@import "static/settings.scss";
+@import "static/mixins.scss";
+
+.gallery {
+  &__title {
+    font-size: $fz-xxxl;
+    font-weight: 700;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 25px;
+  }
+}
+</style>
