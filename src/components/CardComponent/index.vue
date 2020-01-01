20fa9ec400c4318f29bac85edf33f180d31aa4cb
Nicole Marie Jimenez <jas.jimenez411@gmail.com>
2020-01-01 13:07:13 +0100

    feat(#9): Create CardComponent

---
+++ src/components/CardComponent/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="card-component">
+        <span class="card-component__title">
+            <slot name="title"></slot>
+        </span>
+        <div class="card-component__content">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'card-component',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "static/settings.scss";
+@import "static/mixins.scss";
+
+.card-component {
+  width: 328px;
+  height: 397px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  background-color: var(--white);
+  padding: $gt-med;
+  align-items: center;
+
+  &__title {
+    font-size: $fz-l;
+    font-weight: $fw-l;
+  }
+
+  &__content {
+    font-size: $fz-med;
+    font-weight: $fw-med;
+  }
+}
+</style>
