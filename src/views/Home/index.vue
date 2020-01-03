7f1ce683970e2069200c317a69ef953bfe41e172
Nicole Marie Jimenez <jas.jimenez411@gmail.com>
2020-01-03 19:18:06 +0100

    feat(#4): Add stuff to the homepage

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,15 +3,38 @@
         <div class="home__title">
             The Chuck Norris
         </div>
+        <div class="container home__about">
+            <span>Carlos Ray "Chuck" Norris (born March 10, 1940) is an American martial artist, actor, film producer and screenwriter. After serving in the United States Air Force, Norris won many martial arts championships and later founded his own discipline Chun Kuk Do. Norris is a black belt in Tang Soo Do, Brazilian jiu jitsu and Judo. Shortly after, in Hollywood, Norris trained celebrities in martial arts. Norris went on to appear in a minor role in the spy film The Wrecking Crew (1969). </span>
+            <img src="https://4.bp.blogspot.com/-7J8h-PXjINk/V2_MFXxfTWI/AAAAAAAAQa4/CR4Ylm_X0vEuVFI0lysl3_nLdF91xrHbgCLcB/s200/chuck_norris_walker.png" alt="Chuck Norris">
+        </div>
+        <div class="home__joke">
+            <div class="home__joke--inner">
+                <h2>Joke Time</h2>
+                <span>{{info.data.value}}</span>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+    data() {
+        return {
+            info: null
+        }
+    },
     metaInfo() {
         return {
             title: 'Chuck Norris',
         }
+    },
+    mounted() {
+        axios
+            .get('https://api.chucknorris.io/jokes/random')
+            .then(response => (this.info = response))
     },
 }
 </script>
@@ -21,6 +44,9 @@ export default {
 @import "static/mixins.scss";
 
 .home {
+  display: flex;
+  flex-direction: column;
+
   &__title {
     font-size: $fz-xxxl;
     font-weight: 700;
@@ -28,6 +54,21 @@ export default {
     justify-content: center;
     align-items: center;
     padding-top: 25px;
+    margin-bottom: $gt-med;
+  }
+
+  &__about {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  &__joke {
+    background-color: $c-orange-3;
+
+    &--inner {
+        height: 300px;
+    }
   }
 }
 </style>
