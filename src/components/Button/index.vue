a0339b0f4377719c5087015b92371733b7b97483
Nicole Marie Jimenez <jas.jimenez411@gmail.com>
2020-01-04 09:21:16 +0100

    feat(#27): Add onclick animation

--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -1,26 +1,26 @@
 <template>
-    <button class="btn" :disabled="disabled" :class="[`btn__type--${type}`]" @click="handleClick">
-        <slot />
-    </button>
+  <button class="btn" :disabled="disabled" :class="[`btn__type--${type}`]" @click="handleClick">
+    <slot />
+  </button>
 </template>
 <script>
 export default {
-    name: 'gm5-button',
-    props: {
-        disabled: {
-            type: Boolean,
-            default: false,
-        },
-        type: {
-            type: String,
-            default: 'normal',
-        },
+  name: 'gm5-button',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
     },
-    methods: {
-        handleClick(evt) {
-            this.$emit('click', evt)
-        },
+    type: {
+      type: String,
+      default: 'normal',
     },
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt)
+    },
+  },
 }
 </script>
 <style lang="scss" scoped>
@@ -33,6 +33,10 @@ export default {
   color: white;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 30px;
+  transition: transform ease-in 0.1s, box-shadow ease-in 0.25s;
+  cursor: pointer;
+  -webkit-appearance: none;
+  font-size: $fz-med;
 
   &[disabled] {
     cursor: not-allowed;
@@ -43,7 +47,13 @@ export default {
   }
 
   &:focus {
-    outline: none;
+    outline: 0;
+  }
+
+  &:active {
+    position: relative;
+    top: 1px;
+    box-shadow: 0;
   }
 
   &__type {
